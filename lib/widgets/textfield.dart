bf9f27775654bf5303759d738469ae7e3ebb6709
nashva <nashvakizhakkayil@gmail.com>
2024-07-31 21:20:32 +0530

    initial commit

---
+++ lib/widgets/textfield.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CustomTextfield extends StatelessWidget {
+  const CustomTextfield({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Theme.of(context).colorScheme.tertiary,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+          filled: true,
+          fillColor: Colors.white,
+          hintText: 'Email'),
+    );
+  }
+}
